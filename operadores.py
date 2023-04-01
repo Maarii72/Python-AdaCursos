790efd85535677c2fe0abfcb423f08aa72d11dad
Marilena Sales <marilena.sales@gmail.com>
2023-04-01 15:33:25 -0300

    Operadores

---
+++ operadores.py
@@ -0,0 +1,30 @@
+"""
+  divisão:/
+  divisão: inteira: //
+  potência:**
+  resto da divisão %
+"""
+idade1 = 10
+idade2 = 25
+
+numero1 = 10
+numero2= 20
+
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(20 % 3)
+print(2 ** 3)
+
+print(idade1 > idade2)
+print(idade1 <= idade2)
+print('Python' == 'python')
+print('banana' != 'abacaxi')
+print(altura1 >= altura2)
+print(altura2 > altura3)
