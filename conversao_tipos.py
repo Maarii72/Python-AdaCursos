a8328979ce84daff167b0c06e8c4e35b35a00ed0
Marilena Sales <marilena.sales@gmail.com>
2023-04-01 16:09:36 -0300

    conversão de tipos

---
+++ conversao_tipos.py
@@ -0,0 +1,19 @@
+idade = '27'
+numero1 = '10'
+numero2 = '20'
+
+print(idade,type(idade))
+idade_inteira = int(idade)
+print(idade_inteira,type(idade_inteira))
+
+"""
+  int()
+  str()
+  float()
+  boll()
+"""
+
+altura = float(
+                input ('Informe a sua altura: ')
+              )
+print(altura, type(altura))
