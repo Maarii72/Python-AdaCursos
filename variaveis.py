d87f47b2fb019e406ad06afe7b0771d796436b13
Marilena Sales <marilena.sales@gmail.com>
2023-04-01 15:18:14 -0300

    variáveis

---
+++ variaveis.py
@@ -0,0 +1,21 @@
+# Comentários
+
+"""
+varias linhas
+
+"""
+idade = 27 #criando uma variavel
+nome = "Marilena" #criando uma variavel
+estudando = True
+print(type(nome), 
+      type(idade),
+      type(estudando),
+      )
+
+linguagem = input("a linguagem que você está estudando?")
+print("a linguagem que você está estudando? é: ",linguagem)
+
+"""
+str: caracteres
+bool: valores lógicos
+"""
